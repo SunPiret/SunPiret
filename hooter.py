56a76e56fcc94c839f0f187cfc9cf751e5cc43b0
Medvedev Roman <frmanxxx@gmail.com>
2022-08-23 20:47:04 +0300

    del 1 cal

--- hooter.py
+++ hooter.py
@@ -2,11 +2,10 @@ print('FreeWood')
 
 result = 0
 for i in range(1, 16):
-    result += i
+
 
 print(f'Сумма чилел от 1 до 15 = {result}')
 
-list = [6, 7, 8, 'r', 3, 'jg', 4, 5, 7, 2, 5, 7, 9, 4]
+lists = [6, 7, 8, 'r', 3, 'jg', 4, 5, 7, 2, 5, 7, 9, 4]
 
-print(i for i in list)
-
+print(i for i in lists)
