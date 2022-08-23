5adf4376aa17c535d2d1fcb91aefe4df3a8f7fbe
Medvedev Roman <frmanxxx@gmail.com>
2022-08-23 22:16:55 +0300

    revers commit

--- hooter.py
+++ hooter.py
@@ -2,10 +2,11 @@ print('FreeWood')
 
 result = 0
 for i in range(1, 16):
-
+    result += i
 
 print(f'Сумма чилел от 1 до 15 = {result}')
 
-lists = [6, 7, 8, 'r', 3, 'jg', 4, 5, 7, 2, 5, 7, 9, 4]
+list = [6, 7, 8, 'r', 3, 'jg', 4, 5, 7, 2, 5, 7, 9, 4]
 
-print(i for i in lists)
+print(i for i in list)
+
